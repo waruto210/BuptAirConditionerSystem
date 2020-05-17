a2799f42e4f962ae4ec5ad2abb0415d725947391
myc0613 <549664893@qq.com>
2020-05-17 11:00:29 +0800

    adm

--- adm/urls.py
+++ adm/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from .views import set_temp
+from .views import set_temp,checkinfo,setdefault,adm,openclose
 urlpatterns = [
     path('settemp', set_temp),
+    path('',adm),
+    path('checkInfo',checkinfo),
+    path('setdefault',setdefault),
+    path('openclose',openclose)
 ]
