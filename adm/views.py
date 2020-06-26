5330ddb323857885e68669e4f572e5d9906a5f46
myc0613 <549664893@qq.com>
2020-06-26 19:24:38 +0800

    更新set_params数据传输及参数

--- adm/views.py
+++ adm/views.py
@@ -13,19 +13,30 @@ logger = logging.getLogger('collect')
 @login_required(login_url="/login")
 def set_params(request):
     if request.method == 'POST':
+
         work_mode = int(request.POST.get('work_mode', None))
-        sp_mpde = int(request.POST.get('sp_mode', None))
-        goal_temp = int(request.POST.get('goal_temp', None))
+        print(work_mode)
+        sp_mode = int(request.POST.get('sp_mode', None))
+        print(sp_mode)
+
+        goal_temp = int(request.POST.get('env_temp', None))
+        print(goal_temp)
         cold_sub = int(request.POST.get('cold_sub', None))
+        print(cold_sub)
         cold_sup = int(request.POST.get('cold_sup', None))
+        print(cold_sup)
         hot_sub = int(request.POST.get('hot_sub', None))
+        print(hot_sub)
         hot_sup = int(request.POST.get('hot_sup', None))
+        print(hot_sup)
         fee = int(request.POST.get('fee', None))
         print(type(fee))
         max_run = int(request.POST.get('max_run', None))
-        machine.set_params(word_mode=work_mode, sp_mode=sp_mpde, cold_sub=cold_sub, cold_sup=cold_sup,
-                           hot_sub=hot_sub, goal_temp=goal_temp,hot_sup=hot_sup, fee=fee, max_run=max_run)
-        return JsonResponse({'code': '200', 'msg': 'ok'})
+        print(max_run)
+        machine.set_params(word_mode=work_mode, sp_mode=sp_mode, cold_sub=cold_sub, cold_sup=cold_sup,
+                           hot_sub=hot_sub, goal_temp=goal_temp, hot_sup=hot_sup, fee=fee, max_run=max_run)
+        print('set success')
+        return render(request, "adm/setdefault.html")
 
 
 @csrf_exempt
